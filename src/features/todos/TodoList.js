9d9cf34468c76da31ec1ee182fafb42fba841112
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-26 19:51:56 +0300

    todolist and apislice

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,2 +1,55 @@
-const TodoList = () => {};
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+import {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} from "../api/apiSlice";
+
+const TodoList = () => {
+  const [newTodo, setNewTodo] = useState("");
+
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //addtodo
+    setNewTodo("");
+  };
+
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new todo item</label>
+      <div className="new-todo">
+        <input
+          type="text"
+          id="new-todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
+
+  return (
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+    </main>
+  );
+};
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 // import { Provider } from "react-redux";
 // import { store } from "./app/store";
 
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { apiSlice } from "./features/api/apiSlice";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
@@ -12,7 +14,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
 
