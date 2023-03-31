f0cf7eaba344fc5bd93164082f829080cd9591cf
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-31 08:18:11 +0300

    Total completed items logic

--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const TotalCompleteItems = () => {
+  //extract data from the store
+  //below state is my entire redux store
+
+  const completedTodos = useSelector((state) =>
+    state.todos.filter((todo) => todo.completed == true)
+  );
   return <h4 className="mt-3">Total Complete Items: 5</h4>;
 };
 export default TotalCompleteItems;
