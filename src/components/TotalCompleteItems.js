0a171a20cf342f1a3435fed7e8818ac1200177fa
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-31 08:19:42 +0300

    Total completed items logic

--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -8,6 +8,8 @@ const TotalCompleteItems = () => {
   const completedTodos = useSelector((state) =>
     state.todos.filter((todo) => todo.completed == true)
   );
-  return <h4 className="mt-3">Total Complete Items: 5</h4>;
+  return (
+    <h4 className="mt-3">Total Complete Items: {completedTodos.length}</h4>
+  );
 };
 export default TotalCompleteItems;
